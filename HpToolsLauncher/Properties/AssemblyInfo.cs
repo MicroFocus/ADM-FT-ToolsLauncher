9a137c9e574a37c6b5fe97c49c6440dae870962a
Solever Xu <yuanhai.xu@microfocus.com>
2021-11-17 19:33:06 -0800

    Update doc and version for v1.0 beta rev10

--- HpToolsLauncher/Properties/AssemblyInfo.cs
+++ HpToolsLauncher/Properties/AssemblyInfo.cs
@@ -54,6 +54,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.29.221")]
+[assembly: AssemblyVersion("1.0.30.1919")]
 //[assembly: AssemblyFileVersion("1.0")]
 [assembly: AssemblyInformationalVersion("1.0-beta")]
