e35f9eb9c075279f10c83a9920d706a617fcee27
Dorin Bogdan <48766961+dorin7bogdan@users.noreply.github.com>
2026-05-08 17:32:27 +0300

    DEF-1872146 UFT One 25.2, AzureDevops pipeline execution error

--- HpToolsLauncher/TestRunners/GuiTestRunner.cs
+++ HpToolsLauncher/TestRunners/GuiTestRunner.cs
@@ -5,7 +5,7 @@
  * __________________________________________________________________
  * MIT License
  *
- * Copyright 2012-2024 Open Text
+ * Copyright 2012-2026 Open Text
  *
  * The only warranties for products and services of Open Text and
  * its affiliates and licensors ("Open Text") are as may be set forth
@@ -262,14 +262,14 @@ namespace HpToolsLauncher
                 return runDesc;
             }
 
-            if (_qtpApplication.Test != null && _qtpApplication.Test.Modified)
+/*          if (_qtpApplication.Test != null && _qtpApplication.Test.Modified)
             {
                 var message = Resources.QtpNotLaunchedError;
                 errorReason = message;
                 runDesc.TestState = TestState.Error;
                 runDesc.ErrorDesc = errorReason;
                 return runDesc;
-            }
+            }*/
 
             _qtpApplication.UseLicenseOfType(_useUftLicense ? tagUnifiedLicenseType.qtUnifiedFunctionalTesting : tagUnifiedLicenseType.qtNonUnified);
 
