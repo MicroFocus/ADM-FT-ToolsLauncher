2bda181a4b8dae8188be78f65e45ef8eca751c3d
OPENTEXT\adumitras <adumitras@opentext.com>
2026-05-28 16:33:24 +0300

    fix: remove the DLConnection object which has a defect and use tulip connection instead.

--- HpToolsLauncher/TestRunners/GuiTestRunner.cs
+++ HpToolsLauncher/TestRunners/GuiTestRunner.cs
@@ -233,13 +233,6 @@ namespace HpToolsLauncher
                     // set Mc connection and other mobile info into rack if neccesary
                     SetMobileInfo();
 
-                    if (!HandleDigitalLab(qtpVersion, ref errorReason))
-                    {
-                        runDesc.TestState = TestState.Error;
-                        runDesc.ErrorDesc = errorReason;
-                        return runDesc;
-                    }
-
                     if (!_qtpApplication.Launched)
                     {
                         if (_runCancelled())
@@ -963,64 +956,6 @@ namespace HpToolsLauncher
 
         }
 
-
-        private bool HandleDigitalLab(Version qtpVersion, ref string errorReason)
-        {
-            if (_mcConnection == null || _mcConnection.HostAddress.IsNullOrEmpty() || qtpVersion < new Version(2023, 4))
-            {
-                return true;
-            }
-            return SetDLOptions(_qtpApplication.Options.DLConnection, ref errorReason);
-        }
-
-        private bool SetDLOptions(DLConnectionOptions options, ref string errorReason)
-        {
-            try
-            {
-                options.Type = ((int)DigitalLabType.UFT).ToString();
-
-                if (_mcConnection.MobileAuthType == AuthType.AuthToken)
-                {
-                    options.AuthType = AuthType.AuthToken.GetEnumDescription();
-                    options.UserName = _mcConnection.ClientId;
-                    options.Password = GetEncryptedPassword(_mcConnection.SecretKey);
-                }
-                else if (!_mcConnection.UserName.IsNullOrEmpty())
-                {
-                    options.AuthType = AuthType.UsernamePassword.GetEnumDescription();
-                    options.UserName = _mcConnection.UserName;
-                    options.Password = GetEncryptedPassword(_mcConnection.Password);
-                }
-                options.Server = _mcConnection.HostAddress;
-                options.Port = _mcConnection.HostPort;
-                options.UseSSL = _mcConnection.UseSSL;
-
-                if (_mcConnection.UseProxy)
-                {
-                    options.UseProxySettings = true;
-                    options.ProxyType = _mcConnection.ProxyType == 1 ? "System Proxy" : "HTTP Proxy";
-                    options.ProxyAddress = _mcConnection.ProxyAddress;
-                    options.ProxyPort = _mcConnection.ProxyPort.ToString();
-                    if (_mcConnection.UseProxyAuth)
-                    {
-                        options.SpecifyAuthentication = true;
-                        options.ProxyUserName = _mcConnection.ProxyUserName;
-                        options.ProxyPassword = _mcConnection.ProxyPassword;
-                    }
-                }
-
-                options.ShowRemoteWndOnRun = true;
-                options.WorkSpace = _mcConnection.WorkspaceId;
-                ConsoleWriter.WriteLine($"[DL] Using workspace: {options.WorkSpace}");
-
-                return true;
-            }
-            catch (Exception e)
-            {
-                errorReason = e.Message;
-                return false;
-            }
-        }
 
         /// <summary>
         /// stops and closes qtp test, to make sure nothing is left floating after run.
