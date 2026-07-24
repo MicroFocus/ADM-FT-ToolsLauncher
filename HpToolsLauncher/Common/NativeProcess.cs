84a512b6ad39d71dea848a6dff4d23300733c735
Dorin Bogdan <48766961+dorin7bogdan@users.noreply.github.com>
2026-07-24 12:50:38 +0300

    update Copyright year

--- HpToolsLauncher/Common/NativeProcess.cs
+++ HpToolsLauncher/Common/NativeProcess.cs
@@ -5,7 +5,7 @@
  * __________________________________________________________________
  * MIT License
  *
- * Copyright 2012-2024 Open Text
+ * Copyright 2012-2026 Open Text
  *
  * The only warranties for products and services of Open Text and
  * its affiliates and licensors ("Open Text") are as may be set forth
